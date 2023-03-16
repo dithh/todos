52c00a3e73e0135ad4a8cafc4613de3baca777eb
dithh <aleksander.hersztek@gmail.com>
2023-03-16 21:43:39 +0100

    separated routes logic to controller

---
+++ src/todos/controllers/todosController.ts
@@ -0,0 +1,56 @@
+import { Todo } from '../schema'
+import { Request, Response } from 'express'
+
+export const getAllUsers = async (req: Request, res: Response) => {
+    const todos = await Todo.find()
+    res.json(todos)
+}
+
+export const getUserById = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params
+        const todo = await Todo.findById(id)
+        if (todo) {
+            res.json(todo)
+        }
+    } catch (e) {
+        console.error(e)
+        res.status(500)
+        res.json({ message: 'There was an error' })
+    }
+}
+
+export const createUser = async (req: Request, res: Response) => {
+    try {
+        const todo = await new Todo(req.body).save()
+        res.json(todo)
+    } catch (e) {
+        console.error(e)
+        res.status(500)
+        res.json({ message: 'There was an error' })
+    }
+}
+
+export const patchUser = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params
+        const update = req.body
+        console.log(update)
+        const todo = await Todo.findByIdAndUpdate(id, update, { new: true })
+        res.json({ message: `Todo ${id} updated`, data: todo })
+    } catch (e) {
+        console.error(e)
+        res.status(500)
+        res.json({ message: 'There was an error ' })
+    }
+}
+export const deleteUserByid = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params
+        await Todo.findByIdAndDelete(id)
+        res.json({ message: `Todo ${id} deleted` })
+    } catch (e) {
+        res.status(500)
+        res.json({ message: 'There was an error ' })
+    }
+}
--- src/todos/routes/index.ts
+++ src/todos/routes/index.ts
@@ -1,59 +1,22 @@
 import { Router } from 'express'
-import { Todo } from '../schema'
+import {
+    getAllUsers,
+    getUserById,
+    createUser,
+    patchUser,
+    deleteUserByid,
+} from '../controllers/todosController'
 
 export const router = Router()
 
-router.get('/', async (_, res) => {
-    const todos = await Todo.find()
-    res.json(todos)
-})
+router.get('/', getAllUsers)
 
-router.get('/:id', async (req, res) => {
-    try {
-        const { id } = req.params
-        const todo = await Todo.findById(id)
-        if (todo) {
-            res.json(todo)
-        }
-    } catch (e) {
-        console.error(e)
-        res.status(500)
-        res.json({ message: 'There was an error' })
-    }
-})
+router.get('/:id', getUserById)
 
-router.post('/', async (req, res) => {
-    try {
-        const todo = await new Todo(req.body).save()
-        res.json(todo)
-    } catch (e) {
-        console.error(e)
-        res.status(500)
-        res.json({ message: 'There was an error' })
-    }
-})
+router.post('/', createUser)
 
-router.patch('/:id', async (req, res) => {
-    try {
-        const { id } = req.params
-        const update = req.body
-        console.log(update)
-        const todo = await Todo.findByIdAndUpdate(id, update, { new: true })
-        res.json({ message: `Todo ${id} updated`, data: todo })
-    } catch (e) {
-        console.error(e)
-        res.status(500)
-        res.json({ message: 'There was an error ' })
-    }
-})
+router.patch('/:id', patchUser)
 
-router.delete('/:id', async (req, res) => {
-    try {
-        const { id } = req.params
-        await Todo.findByIdAndDelete(id)
-        res.json({ message: `Todo ${id} deleted` })
-    } catch (e) {
-        res.status(500)
-        res.json({ message: 'There was an error ' })
-    }
-})
+router.delete('/:id', deleteUserByid)
+
+// router.put('/:id', putUserById)
